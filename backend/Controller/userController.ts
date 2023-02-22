57e8f353e3070767a30cc973fa662b50935d7695
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-22 18:32:54 +0100

    update

--- backend/Controller/userController.ts
+++ backend/Controller/userController.ts
@@ -68,6 +68,7 @@ export const sendToAnotherWallet = async (req: Request, res: Response) => {
     }); // getting receiver
     const getSender = await userModel.findById(req.params.userId); // getting sender
     const getSenderWallet = await walletModel.findById(getReceiver?._id); // geting Sender Wallet {so a sender(user) can debit from it}
+    const getReceiverWallet = await walletModel.findById(getSender?._id); // geting Recevier Wallet {so a Recevier(user) can creidt to it}
 
     if (getSender && getReceiver) {
       if (amount > getSenderWallet?.balance!) {
@@ -89,6 +90,11 @@ export const sendToAnotherWallet = async (req: Request, res: Response) => {
           new mongoose.Types.ObjectId(createSenderHistory?._id)
         );
         getSender.save();
+
+        await walletModel.findByIdAndUpdate(getReceiver._id, {
+          credit: amount,
+          debit: 0,
+        });
       }
     } else {
     }
