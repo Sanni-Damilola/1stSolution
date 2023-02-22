abbd7aa719ce597160c1a00636bdf199c824060c
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-22 18:47:05 +0100

    update

--- backend/Routes/userRoute.ts
+++ backend/Routes/userRoute.ts
@@ -1,10 +1,17 @@
 import { Router } from "express";
-import { deleteAllUser, registerUser } from "../Controller/userController";
+import {
+  deleteAllUser,
+  getAllUser,
+  registerUser,
+  sendToAnotherWallet,
+} from "../Controller/userController";
 
 // creating User Route
 //  👇👇
 const route = Router();
 route.route("/postUser").post(registerUser); // register User
 route.route("/deleteAll").delete(deleteAllUser); // delete All User
+route.route("/send/:userId/:walletId").patch(sendToAnotherWallet); // delete All User
+route.route("/getall").get(getAllUser); // delete All User
 
 export default route;
