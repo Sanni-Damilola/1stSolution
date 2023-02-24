67e8ade1086834e6ea0b618ad98a567161fe0e56
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-24 23:13:21 +0100

    update

--- backend/Routes/userRoute.ts
+++ backend/Routes/userRoute.ts
@@ -10,7 +10,7 @@ import {
 //  👇👇
 const route = Router();
 route.route("/postUser").post(registerUser); // register User
-route.route("/deleteAll").delete(deleteAll_User_History_And_Wallet); // delete All User,History and Wallet
+route.route("/deleteAll").delete(deleteAll_User_History_And_Wallet); // delete All User, History and Wallet
 route.route("/send/:senderId/:senderWalletId").patch(sendToAnotherWallet); // update 
 route.route("/getall").get(getAllUser); // get All User
 
