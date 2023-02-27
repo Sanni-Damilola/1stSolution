a7c683c7d0957305413f1ad42df8b274e1c303ee
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-27 18:08:55 +0100

    update

--- frontend/src/Components/Home/Header/Header.tsx
+++ frontend/src/Components/Home/Header/Header.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import logo from "../Image/logo.svg";
+import logo from "../../Image/logo.svg";
 
 const Header = () => {
   return (
