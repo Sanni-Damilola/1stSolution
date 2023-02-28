8d1d117204a7227d2fd8dcac561bead89125d605
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-28 15:23:52 +0100

    update

--- frontend/src/Components/Home/HomePage/HomePage.tsx
+++ frontend/src/Components/Home/HomePage/HomePage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../Header/Header";
 import Hero from "../Hero/Hero";
 import Investify from "../Investify/Investify";
+import Meet from "../Meet/Meet";
 import Savings from "../Saveings/Savings";
 import Security from "../Security/Security";
 
@@ -13,6 +14,7 @@ const HomePage = () => {
       <Security />
       <Savings />
       <Investify />
+      <Meet />
     </div>
   );
 };
--- frontend/src/Components/Home/Meet/Meet.tsx
+++ frontend/src/Components/Home/Meet/Meet.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "styled-components";
+import image from "../../Image/pexels-canva-studio-3194524.jpg";
 
 const Meet = () => {
   return (
@@ -14,7 +15,7 @@ const Meet = () => {
           opportunities.
         </p>
       </Right>
-      <Left />
+      <Left src={image} />
     </Container>
   );
 };
@@ -22,5 +23,14 @@ const Meet = () => {
 export default Meet;
 
 const Container = styled.div``;
-const Right = styled.div``;
-const Left = styled.div``;
+const Right = styled.div`
+  width: 60%;
+  height: 100%;
+  background-color: rgb(13, 96, 216);
+  height: 400px;
+`;
+const Left = styled.img`
+  width: 40%;
+  height: 100%;
+  object-fit: cover;
+`;
