45e3d9720f5d3868760354c291c174e7bb1f2417
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-28 13:21:49 +0100

    update

--- frontend/src/Components/Home/Security/Security.tsx
+++ frontend/src/Components/Home/Security/Security.tsx
@@ -1,20 +1,22 @@
 import React from "react";
 import styled from "styled-components";
+import UnderLineButton from "../../Props/UnderLineButton";
+import security from "../../Image/security.png";
 
 const Security = () => {
   return (
     <Container>
       <Wrapper>
-        <Image />
+        <Image src={security} />
         <Wrap>
-          <b>Your security is our priority</b>
+          <Div>Your security is our priority</Div>
           <p>
             PiggyVest uses the highest level of Internet Security and it is
-            secured by 256 bits SSL security encryption to ensure that your
-            information is comepletely protected from fraud.
+            secured by 256 bits SSL <br /> security encryption to ensure that
+            your information is comepletely protected from fraud.
           </p>
+          <UnderLineButton />
         </Wrap>
-        
       </Wrapper>
     </Container>
   );
@@ -22,9 +24,26 @@ const Security = () => {
 
 export default Security;
 
-const Wrapper = styled.div``
-const Image = styled.img``
-const Wrap = styled.div``
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Image = styled.img`
+  width: 100px;
+  margin-right: 30px;
+`;
+const Wrap = styled.div`
+  P {
+    font-size: 16px;
+    font-weight: 600;
+  }
+`;
+
+const Div = styled.div`
+  font-weight: bold;
+  font-size: 30px;
+`;
 
 const Container = styled.div`
   width: 100%;
