b1ba7e0583c57108970d3360f97c12b209da2e34
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-28 14:29:33 +0100

    update

--- frontend/src/Components/Props/SavingsProps.tsx
+++ frontend/src/Components/Props/SavingsProps.tsx
@@ -6,28 +6,57 @@ import { BsArrowRight } from "react-icons/bs";
 interface props {
   text1: string;
   text2: string;
+  iconColor: string;
+  buttoText: string;
+  image;
 }
 
-const SavingsProps = () => {
+const SavingsProps: React.FC<props> = ({
+  text1,
+  text2,
+  iconColor,
+  buttoText,
+  image,
+}) => {
   return (
     <Container>
-      <Image src={lock} />
-      <span>Automated Savings</span>
+      <Image src={image} />
+      <span></span>
       <p>
         Build a dedicated savings faster on your terms automatically or
         manually.
       </p>
-      <ButtonWrapper>
+      <nav>
         <Icon>
           <BsArrowRight />
         </Icon>
         <Text>piggybank</Text>
-      </ButtonWrapper>
+      </nav>
     </Container>
   );
 };
 
 export default SavingsProps;
+
+const Image = styled.img`
+  width: 50px;
+  margin-bottom: 20px;
+`;
+const Icon = styled.div`
+  display: flex;
+  width: 40px;
+  height: 40px;
+  color: blue;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  background-color: rgb(227, 245, 255);
+`;
+const Text = styled.div`
+  color: rgb(53, 53, 53);
+  margin-left: 10px;
+  font-weight: 600;
+`;
 
 const Container = styled.div`
   display: flex;
@@ -38,6 +67,7 @@ const Container = styled.div`
   background-color: #f9f9f9;
   padding-left: 30px;
   height: 340px;
+  cursor: pointer;
   span {
     font-weight: bold;
     font-size: 26px;
@@ -47,28 +77,22 @@ const Container = styled.div`
     vertical-align: baseline;
     text-decoration: none solid rgb(53, 53, 53);
     font-weight: 500;
-    font-size: 15px;
+    font-size: 16px;
     line-height: 27.2px;
     color: rgb(53, 53, 53);
+    max-width: 290px;
   }
-`;
 
-const Image = styled.img`
-  width: 50px;
-  margin-bottom: 20px;
-`;
-const ButtonWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-const Icon = styled.div`
-  /* display: flex;
-justify-content: center;
-align-items: center; */
-  border-radius: 50%;
-  padding: 10px;
-`;
-const Text = styled.div`
-  color: rgb(53, 53, 53);
+  nav {
+    display: flex;
+    align-items: center;
+    margin-top: 50px;
+
+    transition: all 650ms;
+  }
+  :hover {
+    nav {
+      margin-left: 12px;
+    }
+  }
 `;
