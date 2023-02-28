a1b6bcf7ee91b1e2d44783cbdf50248730f0d3b5
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-28 16:06:36 +0100

    update

--- frontend/src/Components/Custom/Customers.tsx
+++ frontend/src/Components/Custom/Customers.tsx
@@ -1,17 +1,44 @@
 import React from "react";
 import styled from "styled-components";
+import ButtonLinkToApp from "../Props/ButtonLinkToApp";
+import appleIcon from "../../Image/apple-icon.svg";
+import googleIcon from "../../Image/google-icon.svg";
+
 
 const Customers = () => {
   return (
     <Container>
-      <Replies></Replies>
-      <Left></Left>
-      <Wrapper></Wrapper>
+      <Wrapper>
+        <Replies></Replies>
+        <Left>
+          <ButtonWrapper>
+            <ButtonLinkToApp
+              image={appleIcon}
+              padding="12px 18px"
+              text="Get on iPhone"
+            />
+            <ButtonLinkToApp
+              image={googleIcon}
+              padding="12px 15px"
+              text="Get on Android"
+            />
+          </ButtonWrapper>
+        </Left>
+      </Wrapper>
     </Container>
   );
 };
 
 export default Customers;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  justify-content: space-between;
+  width: 63%;
+  height: 100px;
+`;
 
 const Container = styled.div`
   display: flex;
