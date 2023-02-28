acefad0636ae73570f9b0da32aa64b2ff51edc7e
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-28 16:41:26 +0100

    update

--- frontend/src/Components/Props/CustomersReply.tsx
+++ frontend/src/Components/Props/CustomersReply.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import styled from "styled-components";
+import image from "../Image/adebola.jpeg";
 
 const CustomersReply = () => {
   return (
     <Container>
-      <Date></Date>
+      <Date>hursday, 27th of October 2022 by 13:03 PM </Date>
       <Wrapper>
-        <Image />
+        <Image src={image} />
         <Left>
-          <Name></Name>
-          <Message></Message>
+          <Name>Adebola P</Name>
+          <Message>
+            avenue for me to do so. Simple to use, very friendly interface and I
+            just love the fact that they answer my mails immediately. Love the
+            energy!!!
+          </Message>
         </Left>
       </Wrapper>
     </Container>
@@ -19,11 +24,15 @@ const CustomersReply = () => {
 export default CustomersReply;
 
 const Container = styled.div`
-display: flex;
-flex-direction: column;
-
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding-left: 20px;
 `;
-const Date = styled.span``;
+const Date = styled.span`
+  color: rgb(0, 0, 0, 0.5);
+  font-size: 14px;
+`;
 const Wrapper = styled.div``;
 const Image = styled.img``;
 const Left = styled.div``;
