cf64f0d374c6f015202e1415b8aca764cce76802
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-28 12:54:59 +0100

    update

--- frontend/src/Components/Props/ButtonLinkToApp.tsx
+++ frontend/src/Components/Props/ButtonLinkToApp.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import styled from "styled-components";
+import appIcon from "../../Image/apple-icon.svg"
 
 const ButtonLinkToApp = () => {
-  return <Button>
-    <Icon>
+  return (
+    <Button>
+      <Icon />
 
-    </Icon>
-    <Text>
-      Get on iPhone
-    </Text>
-  </Button>;
+      <Text>Get on iPhone</Text>
+    </Button>
+  );
 };
 
 export default ButtonLinkToApp;
 
 const Button = styled.div``;
+const Icon = styled.img``;
+const Text = styled.div``;
