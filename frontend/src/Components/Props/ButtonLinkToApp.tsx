bff04bd66e496afa2928f53e5bd8086d7c1c0966
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-28 13:02:25 +0100

    update

--- frontend/src/Components/Props/ButtonLinkToApp.tsx
+++ frontend/src/Components/Props/ButtonLinkToApp.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import styled from "styled-components";
-import appIcon from "../../Image/apple-icon.svg"
+import appIcon from "../Image/apple-icon.svg";
 
-const ButtonLinkToApp = () => {
+interface props {
+  text: string;
+  image: string;
+}
+
+const ButtonLinkToApp: React.FC<props. = ({ padding, image }) => {
   return (
-    <Button>
-      <Icon />
-
+    <Button padding={padding}>
+      <Icon src={appIcon} />
       <Text>Get on iPhone</Text>
     </Button>
   );
@@ -14,6 +18,29 @@ const ButtonLinkToApp = () => {
 
 export default ButtonLinkToApp;
 
-const Button = styled.div``;
-const Icon = styled.img``;
+const Button = styled.div<{ padding }>`
+  padding: ${(props) => props.padding};
+  border: 1px solid rgb(0, 0, 0, 0.2);
+  outline: none;
+  cursor: pointer;
+  margin-right: 10px;
+  border-radius: 15px;
+  transition: all 360ms;
+  color: rgb(12, 24, 37);
+  background-color: white;
+  font-size: 16.5px;
+  font-weight: 500;
+  width: fit-content;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  :hover {
+    margin-top: -2px;
+  }
+`;
+const Icon = styled.img`
+  margin-right: 6px;
+  margin-bottom: 3px;
+`;
 const Text = styled.div``;
