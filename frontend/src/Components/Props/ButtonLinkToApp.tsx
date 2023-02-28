8de4b7ea9b9b1e9729bb004aa72a72b6b56b5251
Sanni-Damilola <sannifortune11@gmail.com>
2023-02-28 11:17:39 +0100

    update

--- frontend/src/Components/Props/ButtonLinkToApp.tsx
+++ frontend/src/Components/Props/ButtonLinkToApp.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 
 const ButtonLinkToApp = () => {
-  return (
-    <div>ButtonLinkToApp</div>
-  )
-}
+  return <Button>ButtonLinkToApp</Button>;
+};
 
-export default ButtonLinkToApp
+export default ButtonLinkToApp;
+
+const Button = styled.div``;
