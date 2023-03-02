0b2dcb60078573f2b759ede937287cb55c75fb07
Sanni-Damilola <sannifortune11@gmail.com>
2023-03-02 11:25:13 +0100

    update

--- frontend/src/Components/DashBoard/DashBoard.tsx
+++ frontend/src/Components/DashBoard/DashBoard.tsx
@@ -1,12 +1,29 @@
-import React from 'react'
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const DashBoard = () => {
-  return <Container>
+  return (
+    <Container>
+      <MenuBar></MenuBar>
+      <ManinDasboard></ManinDasboard>
+    </Container>
+  );
+};
 
-  </Container>;
-}
+export default DashBoard;
 
-export default DashBoard
-
-const Container = styled.div``
+const Container = styled.div`
+  width: 100%;
+  height: fit-content;
+  display: flex;
+`;
+const MenuBar = styled.div`
+  width: 260px;
+  height: 100vh;
+  fdc
+  background-color: rgb(6, 40, 99);
+`;
+const ManinDasboard = styled.div`
+  background-color: green;
+  width: calc(100% - 260px);
+`;
