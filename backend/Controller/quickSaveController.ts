003f6ed6c63f7a08d90b880c0d72c42d52b3fc74
Sanni-Damilola <sannifortune11@gmail.com>
2023-03-02 21:05:50 +0100

    update

--- backend/Controller/quickSaveController.ts
+++ backend/Controller/quickSaveController.ts
@@ -1,29 +1,29 @@
-import QuickSave from "../Model/QuickSave";
+import quickSaveModel from "../QuickSaveModel/quickSaveModel";
 import { Request, Response } from "express";
-import WalletModel from "../Model/WalletModel";
+import walletModel from "../WalletModel/walletModel";
 import mongoose from "mongoose";
 
 export const CreateQuickSave = async (req: Request, res: Response) => {
   try {
     const { amount } = req.body;
-    const getWallet = await WalletModel.findById(req.params.walletId);
+    const getWallet = await walletModel.findById(req.params.walletId);
 
     const newDate = new Date().toDateString();
 
-    if (amount > getWallet!.Balance) {
+    if (amount > getWallet!.balance) {
       return res.status(404).json({
         message: "insufficient funds",
       });
     } else {
-      const creating = await QuickSave.create({
+      const creating = await quickSaveModel.create({
         amount,
         autoSave: false,
         dateTime: newDate,
         interest: 0.1,
       });
 
-      await WalletModel.findByIdAndUpdate(getWallet?._id, {
-        Balance: getWallet?.Balance! - amount,
+      await walletModel.findByIdAndUpdate(getWallet?._id, {
+        balance: getWallet?.balance! - amount,
       });
 
       getWallet?.quickSave?.push(new mongoose.Types.ObjectId(creating?._id));
@@ -43,11 +43,11 @@ export const CreateQuickSave = async (req: Request, res: Response) => {
 
 export const ClaimDailyInterest = async (req: Request, res: Response) => {
   try {
-    const getSaveId = await QuickSave.findById(req.params.SaveId);
+    const getSaveId = await quickSaveModel.findById(req.params.SaveId);
 
     const dailyinterest = 0.1 * (1 / 365);
 
-    await QuickSave.findByIdAndUpdate(getSaveId?._id, {
+    await quickSaveModel.findByIdAndUpdate(getSaveId?._id, {
       amount: getSaveId?.amount! * dailyinterest + getSaveId?.amount!,
     });
 
