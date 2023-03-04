ad7428ae5554ae0f9dd9c795ada086f6f84ae287
Sanni-Damilola <sannifortune11@gmail.com>
2023-03-04 15:26:53 +0100

    update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,25 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import DashBoard from "./Components/DashBoard/DashBoard";
+import { useAppSelector } from "./Components/Global/ReduxState/Store";
 import HomeRoutes from "./Components/Routes/HomeRoutes";
+import { GetOneUser } from "./Components/Api/Api";
 
 function App() {
-  return (
-    <div>
-      <HomeRoutes />
-      {/* <DashBoard /> */}
-    </div>
-  );
+  // import img from "../Assets/person.png";
+
+  const user = useAppSelector((state) => state.currentUser);
+
+  console.log(user);
+
+  const fetchUser = useQuery({
+    queryKey: ["user"],
+    queryFn: () => GetOneUser(user?._id),
+  });
+
+  console.log(fetchUser);
+
+  return <div>{user?.name ? <DashBoard /> : <HomeRoutes />}</div>;
 }
 
 export default App;
