18d8c4d957e02c83d38d40c713b4f59d9659122f
Sanni-Damilola <sannifortune11@gmail.com>
2023-03-04 12:26:51 +0100

    update

--- frontend/src/Components/Api/Api.tsx
+++ frontend/src/Components/Api/Api.tsx
@@ -1,8 +1,36 @@
-import { IUser } from "../interface/interface";
 import axios from "axios";
+import { UserData } from "../interface/interface";
 
-const localUrl = "http://loaclhost/api";
+const localUrl = "http://localhost:6400";
 
-export const register = async (data: IUser) => {
-  await axios.post(`${localUrl}/postUser`, data).then((res) => res.data);
+export const createUser = async ({
+  name,
+  email,
+  password,
+  userName,
+  phoneNumber,
+}: any) => {
+  return await axios
+    .post(`${localUrl}/api/user/register`, {
+      name,
+      email,
+      password,
+      userName,
+      phoneNumber,
+    })
+    .then((res) => {
+      return res.data;
+    });
+};
+
+export const GetOneUser = async (id: any) => {
+  return await axios
+    .get(`${localUrl}/api/user/${id}/userdetail`)
+    .then((res) => res.data);
+};
+
+export const TransferMoney = async (data: any, id: any) => {
+  return await axios
+    .post(`${localUrl}/api/user/sendmoney/${id}/${id}`, data)
+    .then((res) => res.data);
 };
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,6 +4,16 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux/es/exports";
+import { Store } from "./Components/Global/ReduxState/Store";
+import { QueryClient } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
+
+const queryClient = new QueryClient();
+let persitstore = persistStore(Store);
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -11,12 +21,17 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={Store}>
+        <PersistGate persistor={persitstore}>
+          <QueryClientProvider client={queryClient}>
+            <App />
+            <ReactQueryDevtools />
+          </QueryClientProvider>
+        </PersistGate>
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
-
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
